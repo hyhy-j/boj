043b90804f817d8aa5c3e68182cc7c0dc7cdea35
hyeon-jeong-leee <lhjlhj10088@gmail.com>
2024-02-02 01:00:19 +0900

    Create 백준1001,백준10998,백준1008백준10869.cpp

---
+++ c++/백준1001,백준10998,백준1008백준10869.cpp
@@ -0,0 +1,18 @@
+// 백준1000, 백준1001, 백준10998, 백준10869
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	// double c;
+	cin >> a >> b;
+	cout << a + b << endl; // 백준1000
+	cout << a - b << endl; // 백준1001
+	cout << a * b << endl; // 백준10998
+	// c = a / b;
+	// printf("%.10f\n", c); // 백준1008
+	cout << a / b << endl;
+	cout << a % b << endl; // 백준10869
+}
