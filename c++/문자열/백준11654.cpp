88ba37285397b8ae623b40749ca1123d9d30fd1b
hyeon-jeong-leee <lhjlhj10088@gmail.com>
2024-02-08 13:42:17 +0900

    Create 백준11654.cpp

---
+++ c++/문자열/백준11654.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <typeinfo>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	char c;
+	cin >> c;
+	
+	cout << int(c);
+}
